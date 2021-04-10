c8aa4236c4ee503dbd22512405fe460927bc36d6
jaehyun Jeong <us0410636@gmail.com>
2021-04-10 20:39:29 +0900

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -84,7 +84,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#4470ff`,
         display: `minimal-ui`,
-        icon: `content/assets/ME.JPG`,
+        icon: `content/assets/J.png`,
       },
     },
     `gatsby-plugin-react-helmet`,
